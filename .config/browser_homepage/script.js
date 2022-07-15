6ef70bb72f7f37f06f3c427910646dec19fa0d21
Afonso <joaquimafonsocunha@gmail.com>
2022-07-15 14:06:11 +0100

    Changed browser homepage (WIP)

--- .config/browser_homepage/script.js
+++ .config/browser_homepage/script.js
@@ -33,21 +33,3 @@ function datetime() {
 
 datetime();
 
-// This function searches for your keyword when you press "Enter"
-// Notice there's no search icon
-document
-  .getElementById("searchbar")
-  .addEventListener("keyup", function (event) {
-    // The keywords which will be searched for
-    let str = document.getElementById("searchbar").value;
-    // The search engine which will be used
-    let search_engine = "https://google.com/";
-
-    // If user presses "enter"
-    // If the string is not equal to NULL and has anything except whitespace
-    // Then search for the keyword
-
-    if (event.keyCode === 13)
-      if (str !== null && str.match(/^ *$/) === null)
-        window.open(search_engine + str, "_self");
-  });
